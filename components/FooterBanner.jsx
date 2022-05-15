666016161a4bce485e15961749bf9344a3f075ef
Pawel <pawel.aleksander@gmail.com>
2022-05-15 11:54:00 +0200

    adding footer

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
 
-const FooterBanner = () => {
+const FooterBanner = ({footerBanner}) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+
+        </div>
+        <div className='right'>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
