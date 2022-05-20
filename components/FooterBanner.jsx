d1af20603892c20dc7abc73d491dcd03d12549d2
Pawel <pawel.aleksander@gmail.com>
2022-05-20 14:30:15 +0200

    adding progres

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import Link from 'next/link';
 import { urlFor } from '../lib/client';
 
-const FooterBanner = ({footerBanner}) => {
+const FooterBanner = ({footerBanner: {discount, largeText1, largeText2, saleTime, smallText, midText, product, desc, buttonText, image}}) => {
   return (
-    <div className='footer-banner-container'>
-      <div className='banner-desc'>
-        <div className='left'>
-
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
         </div>
-        <div className='right'>
-          
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
         </div>
+        <img src={urlFor(image)} alt="zd" className="footer-banner-image" />
       </div>
     </div>
-  )
+  );
 }
 
 export default FooterBanner
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -11,11 +11,11 @@ const HeroBanner = ({ heroBanner }) => {
         <h1>{heroBanner.largeText1}</h1>
         <img
           src={urlFor(heroBanner.image)}
-          alt="koparrrrr"
+          alt="ko"
           className="hero-banner-image"
         />
         <div>
-          <Link href={`/product/ID/${heroBanner.product}`}>
+          <Link href={`/product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
